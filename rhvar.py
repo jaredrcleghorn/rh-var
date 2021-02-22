578493b8c261667d5c3607223a38a4ac7141fbee
Jared Cleghorn <jaredrcleghorn@gmail.com>
2021-02-21 23:14:27 -0600

    Add price plotting

--- rhvar.py
+++ rhvar.py
@@ -1,4 +1,6 @@
 import json
+import matplotlib.pyplot as plt
+import numpy as np
 import robinhood as rh
 
 CONFIG_FILENAME = 'config.json'
@@ -28,7 +30,24 @@ print()
 print('Positions:')
 print()
 
-for position in robinhood.get_positions():
-    print(f"{position['symbol']} ({position['quantity']})")
+positions = robinhood.get_positions()
+
+for i, position in enumerate(positions):
+    print(f"{i + 1}. {position['symbol']} ({position['quantity']})")
+
+print()
+print('Please enter one of the numbers above: ', end='')
+
+symbol = positions[int(input()) - 1]['symbol']
+historicals = robinhood.get_historicals(symbol)
+x = tuple(historical['begins_at'] for historical in historicals)
+y = tuple(historical['close_price'] for historical in historicals)
+
+plt.title(symbol)
+plt.xlabel('Day')
+plt.ylabel('Price')
+plt.xticks(np.linspace(0, len(historicals) - 1, 5))
+plt.plot(x, y)
+plt.show()
 
 print()
